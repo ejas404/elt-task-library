1c2f29e00e48ccf0482e645bd4b63258d518f5e6
ejas404 <ijasjaaass004@gmail.com>
2024-06-15 16:20:49 +0530

    feat: create signup api on auth controller #5

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateAuthorDto } from 'src/author/dto/create-author.dto';
+import { AuthService } from './auth.service';
 
 @Controller('')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService : AuthService){}
+
+    @Post('signup')
+    async signup(@Body() authData : CreateAuthorDto){  
+        return await this.authService.signup(authData)
+    }
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { AuthorService } from 'src/author/author.service';
+import { CreateAuthorDto } from 'src/author/dto/create-author.dto';
+
+type SignUpResponse = {success : boolean, msg : string}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private authorService : AuthorService){}
+
+    async signup(data: CreateAuthorDto): Promise<SignUpResponse> {
+       const user = await this.authorService.createAuthor(data)
+       return {success : true, msg : "Sign up successfully"}
+    }
+}

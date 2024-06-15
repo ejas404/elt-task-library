0c7de557d703dd3d3cd49ca919e54d419f19e8ca
ejas404 <ijasjaaass004@gmail.com>
2024-06-16 02:13:05 +0530

    feat: add delete book api #3

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Param, Post, UseInterceptors } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { BookService } from './book.service';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('book')
-export class BookController {}
+export class BookController {
+
+    constructor(private bookService: BookService) { }
+
+    @Post('create')
+    async create(@Body() book: CreateBookDto) {
+        return await this.bookService.createBook(book)
+    }
+
+    @Delete(':id')
+    async delete(@Param() params : {id : string}) {
+        const {id} = params;
+        return await this.bookService.deleteBook(id)
+    }
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -9,16 +9,22 @@ import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class BookService {
-    constructor(@InjectModel(Book.name) private bookModel : Model<Book>){}
-
-    async createBook(book : CreateBookDto){
-        const {title} = book
-        const isExist = await this.bookModel.findOne({title})
-        if(isExist) throw new BadRequestException('book with same title already exists');
+    constructor(@InjectModel(Book.name) private bookModel: Model<Book>) { }
+    async createBook(book: CreateBookDto) {
+        const { title } = book
+        const isExist = await this.bookModel.findOne({ title })
+        if (isExist) throw new BadRequestException('book with same title already exists');
 
         book['id'] = uuidv4();
         const newBook = await this.bookModel.create(book);
-        const bookResponse = plainToInstance(BookResponseDto, newBook)
+        const bookResponse = plainToInstance(BookResponseDto, newBook, { excludeExtraneousValues: true })
+        return bookResponse;
+    }
+
+    async deleteBook(id : string){
+        const deletedBoook = await this.bookModel.findOneAndDelete({id})
+        if(!deletedBoook) throw new BadRequestException('ivalid id or no book with provided id');
+        const bookResponse = plainToInstance(BookResponseDto, deletedBoook, { excludeExtraneousValues: true });
         return bookResponse;
     }
 }

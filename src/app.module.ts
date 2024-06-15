960a1324f9c552672dd37309d7542ef1a275519f
ejas404 <ijasjaaass004@gmail.com>
2024-06-15 14:51:23 +0530

    feat: generate auth module based on #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath : '.env',isGlobal : true}),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    AdminModule
+    AdminModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],

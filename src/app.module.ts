333082f986e8bdab47867f874cae6c957059801c
ejas404 <ijasjaaass004@gmail.com>
2024-06-15 15:00:16 +0530

    feat: generate author module based on #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath : '.env',isGlobal : true}),
     MongooseModule.forRoot(process.env.MONGO_URI),
     AdminModule,
-    AuthModule
+    AuthModule,
+    AuthorModule
   ],
   controllers: [AppController],
   providers: [AppService],

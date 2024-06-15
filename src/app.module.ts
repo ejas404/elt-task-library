e3486822dd49d86302adc85f8c13b332fdfc2cf4
ejas404 <ijasjaaass004@gmail.com>
2024-06-15 14:23:46 +0530

    feat: create admin module, controller, service based on #4

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath : '.env',isGlobal : true}),
-    MongooseModule.forRoot(process.env.MONGO_URI)
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AdminModule
   ],
   controllers: [AppController],
   providers: [AppService],
